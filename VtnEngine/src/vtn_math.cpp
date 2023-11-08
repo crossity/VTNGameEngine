ffad48bd01c5a65196d8f08fed0f042e9c1c0a4e
OmegaMD <150187711+OmegaMD@users.noreply.github.com>
2023-11-09 00:04:03 +0300

    Update vtn_math.cpp

--- VtnEngine/src/vtn_math.cpp
+++ VtnEngine/src/vtn_math.cpp
@@ -1,28 +1,29 @@
 #include <cmath>
+
 #include "vtn_types.hpp"
 
-float VectorLength(vtnVEC2 &a) {
+float vtnVecLen(vtnVEC2 &a) {
 	return sqrt(a.x * a.x + a.y * a.y);
 }
-float VectorLength(vtnVEC3 &a) {
+float vtnVecLen(vtnVEC3 &a) {
 	return sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
 }
 
-float DotProduct(vtnVEC2 &a, vtnVEC2 &b) {
+float vtnDotProduct(vtnVEC2 &a, vtnVEC2 &b) {
 	return a.x * b.x + a.y * b.y;
 }
-float DotProduct(vtnVEC3 &a, vtnVEC3 &b) {
+float vtnDotProduct(vtnVEC3 &a, vtnVEC3 &b) {
 	return a.x * b.x + a.y * b.y + a.z * b.z;
 }
 
-float VectorAngle(vtnVEC2 &a, vtnVEC2 &b) {
-	return DotProduct(a, b) / (VectorLength(a) * VectorLength(b));
+float vtnVecAngle(vtnVEC2 &a, vtnVEC2 &b) {
+	return vtnDotProduct(a, b) / (vtnVecLen(a) * vtnVecLen(b));
 }
-float VectorAngle(vtnVEC3 &a, vtnVEC3 &b) {
-	return DotProduct(a, b) / (VectorLength(a) * VectorLength(b));
+float vtnVecAngle(vtnVEC3 &a, vtnVEC3 &b) {
+	return vtnDotProduct(a, b) / (vtnVecLen(a) * vtnVecLen(b));
 }
 
-vtnVEC3 CrossProduct(vtnVEC3 &a, vtnVEC3 &b) {
+vtnVEC3 vtnCrossProduct(vtnVEC3 &a, vtnVEC3 &b) {
     vtnVEC3 c;
     c.x = a.y * b.z - a.z * b.y;
     c.y = a.z * b.x - a.x * b.z;
@@ -30,7 +31,7 @@ vtnVEC3 CrossProduct(vtnVEC3 &a, vtnVEC3 &b) {
     return c;
 }
 
-vtnMAT3X3 Transpose(vtnMAT3X3 &a) {
+vtnMAT3X3 vtnTranspose(vtnMAT3X3 &a) {
     vtnMAT3X3 b;
     for (int y = 0; y < 3; y++)
         for (int x = 0; x < 3; x++)
