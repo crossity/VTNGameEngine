f3fc15942ec352dc278840e323ed11c520c511e4
crossity <93277602+crossity@users.noreply.github.com>
2023-11-09 18:34:29 +0300

    Add files via upload

--- VtnEngine/src/vtn.hpp
+++ VtnEngine/src/vtn.hpp
@@ -1,6 +1,7 @@
 #include "vtn_defines.hpp"
 #include "vtn_types.hpp"
+#include "vtn_math.hpp"
 #include "vtn_window.hpp"
 #include "vtn_event.hpp"
 #include "vtn_renderer.hpp"
-#include "vtn_math.hpp"
+#include "vtn_draw.hpp"
---
+++ VtnEngine/src/vtn_draw.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "vtn_types.hpp"
+
+void vtnDrawTriangle(vtnVEC2 a, vtnVEC2 b, vtnVEC2 c, vtnVEC3 color);
